baa6eb044f88cb1b321a148841b5313f901db69f
liuyuanxiao <liuyuanxiao@benmu-health.com>
2018-03-14 15:29:26 +0800

    first BMPlugAmap plugin

---
+++ src/main/java/com/plugamap/EventGeo.java
@@ -0,0 +1,47 @@
+package com.plugamap;
+
+import android.content.Context;
+
+import com.benmu.framework.manager.ManagerFactory;
+import com.benmu.framework.manager.impl.dispatcher.DispatchEventManager;
+import com.benmu.framework.utils.JsPoster;
+import com.benmu.wxbase.EventGate;
+import com.plugamap.manager.GeoManager;
+import com.plugamap.model.GeoResultBean;
+import com.squareup.otto.Subscribe;
+import com.taobao.weex.bridge.JSCallback;
+
+/**
+ * Created by Carry on 2017/11/17.
+ */
+
+public class EventGeo extends EventGate {
+    private JSCallback mCallback;
+    private GeoManager mGeoManager;
+
+    @Override
+    public void perform(Context context, JSCallback jscallback) {
+        getLocation(context, jscallback);
+    }
+
+    public void getLocation(Context context, JSCallback jscallback) {
+        this.mCallback = jscallback;
+        mGeoManager = ManagerFactory.getManagerService(GeoManager.class);
+        mGeoManager.startLocation(context);
+        ManagerFactory.getManagerService(DispatchEventManager.class).getBus().register(this);
+    }
+
+    @Subscribe
+    public void onLocation(GeoResultBean bean) {
+        if (bean.resCode == 0) {
+            //定位成功
+            JsPoster.postSuccess(bean.getData(), mCallback);
+        } else {
+            //定位失败
+            JsPoster.postFailed(mCallback);
+        }
+        mGeoManager.onPause();
+        ManagerFactory.getManagerService(DispatchEventManager.class).getBus().unregister(this);
+    }
+
+}
---
+++ src/main/java/com/plugamap/model/GeoResultBean.java
@@ -0,0 +1,40 @@
+package com.plugamap.model;
+
+import com.benmu.framework.model.BaseResultBean;
+
+/**
+ * Created by Carry on 2017/11/17.
+ */
+
+public class GeoResultBean extends BaseResultBean {
+    private Geo data;
+
+    public class Geo {
+        private double locationLat;
+        private double locationLng;
+
+        public double getLocationLat() {
+            return locationLat;
+        }
+
+        public void setLocationLat(double locationLat) {
+            this.locationLat = locationLat;
+        }
+
+        public double getLocationLng() {
+            return locationLng;
+        }
+
+        public void setLocationLng(double locationLng) {
+            this.locationLng = locationLng;
+        }
+    }
+
+    public Geo getData() {
+        return data;
+    }
+
+    public void setData(Geo data) {
+        this.data = data;
+    }
+}
